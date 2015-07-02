17f2d565c3992efd7dcf5aac6df6ffbf6645aa70
amoses016 <amoses016@gmail.com>
2015-07-02 11:15:40 -0500

    Added properties 'name', 'description', and 'created' to Genre model

--- app/models/genre.server.model.js
+++ app/models/genre.server.model.js
@@ -7,11 +7,40 @@ var mongoose = require('mongoose'),
 	Schema = mongoose.Schema;
 
 /**
+ * Validation
+ */
+function validateLength (v) {
+  // a custom validation function for checking string length to be used by the model
+  return v.length <= 15;
+}
+
+/**
  * Genre Schema
  */
 var GenreSchema = new Schema({
-	// Genre model fields   
-	// ...
+    // the property name
+    created: {         
+        // types are defined e.g. String, Date, Number (http://mongoosejs.com/docs/guide.html)
+        type: Date,   
+        // default values can be set
+        default: Date.now 
+    },
+    description: {
+        type: String,
+        default: '',
+        // types have specific functions e.g. trim, lowercase, uppercase (http://mongoosejs.com/docs/api.html#schema-string-js)
+        trim: true
+    },
+    name: {
+        type: String,
+        default: '',
+        trim: true,     
+        unique : true,
+        // make this a required field
+        required: 'name cannot be blank',
+        // wires in a custom validator function (http://mongoosejs.com/docs/api.html#schematype_SchemaType-validate).
+        validate: [validateLength, 'name must be 15 chars in length or less']
+    }
 });
 
 mongoose.model('Genre', GenreSchema);
