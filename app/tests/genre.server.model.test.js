0615ec96cc771d1e8113eb865b6af397179daf54
amoses016 <amoses016@gmail.com>
2015-07-02 11:09:15 -0500

    Removed auto-generated tests and added relevant ones

--- app/tests/genre.server.model.test.js
+++ app/tests/genre.server.model.test.js
@@ -4,52 +4,22 @@
  * Module dependencies.
  */
 var should = require('should'),
-	mongoose = require('mongoose'),
-	User = mongoose.model('User'),
-	Genre = mongoose.model('Genre');
-
-/**
- * Globals
- */
-var user, genre;
+    mongoose = require('mongoose'),
+    Genre = mongoose.model('Genre');
 
 /**
  * Unit tests
  */
-describe('Genre Model Unit Tests:', function() {
-	beforeEach(function(done) {
-		user = new User({
-			firstName: 'Full',
-			lastName: 'Name',
-			displayName: 'Full Name',
-			email: 'test@test.com',
-			username: 'username',
-			password: 'password'
-		});
+describe('Genre Model', function() {
 
-		user.save(function() { 
-			genre = new Genre({
-				// Add model fields
-				// ...
-			});
+    describe('Saving', function() {
+        it('saves new record');
 
-			done();
-		});
-	});
+        it('throws validation error when name is empty');
 
-	describe('Method Save', function() {
-		it('should be able to save without problems', function(done) {
-			return genre.save(function(err) {
-				should.not.exist(err);
-				done();
-			});
-		});
-	});
+        it('throws validation error when name longer than 15 chars');
 
-	afterEach(function(done) { 
-		Genre.remove().exec();
-		User.remove().exec();
+        it('throws validation error for duplicate genre name');
+    });
 
-		done();
-	});
 });
