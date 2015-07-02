b4a9655dba56faed40944d186aebf24b5b54856a
amoses016 <amoses016@gmail.com>
2015-07-02 11:31:59 -0500

    Created template controller for genres

---
+++ app/controllers/genres.server.controller.js
@@ -0,0 +1,42 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+    _ = require('lodash');
+
+/**
+ * Create a Genre
+ */
+exports.create = function(req, res) {
+
+};
+
+/**
+ * Show the current Genre
+ */
+exports.read = function(req, res) {
+
+};
+
+/**
+ * Update a Genre
+ */
+exports.update = function(req, res) {
+
+};
+
+/**
+ * Delete an Genre
+ */
+exports.delete = function(req, res) {
+
+};
+
+/**
+ * List of Genres
+ */
+exports.list = function(req, res) {
+
+};
